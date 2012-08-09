69fab4550cbda6bf3d9c86e74cfaf946fee68306
sparkinson <samuel.d.parkinson@googlemail.com>
2012-08-09 14:29:22 +0100

    fixed boundary conditions fixed parallel running (still shows no speed up) added timers

--- Sediment_preAssemble.py
+++ Sediment_preAssemble.py
@@ -5,23 +5,23 @@ import Sediment_MMSFunctions as mms
 parameters["std_out_all_processes"] = False;
 parameters["form_compiler"]["cpp_optimize"] = True
 parameters["form_compiler"]["quadrature_degree"] = 8
-# parameters["num_threads"] = 1;
+parameters["num_threads"] = 2
 
 # simulation parameters
-dt_store = 1e-10
+dt_store = 1e-1
 shape_U = 2
 shape_P = 1
 shape_C = 1 
-picard_tol = 1e-5
-CFL = 0.2
-min_dt = 1e-5
+picard_tol = 1e1
+CFL = 0.5
+min_dt = 1e-4
 max_dt = 1e-1
-nl_its = 2
-dX = 5e-3
-T = 20.0
+nl_its = 1
+dX = 0.5e-2
+T = 45.0
 nu_scale_ = 0.20
 beta_ = 4./2.
-L = 2.0
+L = 3.0
 R = 2.217
 d = 25e-6
 conc = 3.49e-03
@@ -50,10 +50,10 @@ g = Constant(9.81)
 u_sink = Constant(5.5e-4)
 
 # save files
-u_file = File("results/u_fine_test.pvd") 
-p_file = File("results/p_fine_test.pvd") 
-c_file = File("results/c_fine_test.pvd")
-c_d_file = File("results/c_d_fine_test.pvd")
+u_file = File("results/u.pvd") 
+p_file = File("results/p.pvd") 
+c_file = File("results/c.pvd")
+c_d_file = File("results/c_d.pvd")
 
 # generate expressions for initial conditions, boundary conditions and source terms
 u_s = Expression(('0.0', '0.0'), degree = shape_U + 1)
@@ -122,7 +122,11 @@ for i in range(mesh.num_vertices()):
 
 free_slip = Expression('0.0', degree = shape_U + 1)
 no_slip = Expression(('0.0','0.0'), degree = shape_U + 1)
-bcu  = [DirichletBC(V, no_slip, "on_boundary")]
+# bcu  = [DirichletBC(V, no_slip, "on_boundary")]
+bcu  = [DirichletBC(V, no_slip, "on_boundary && (near(x[0], 0.0) || near(x[0], " + str(L) + "))"),
+        DirichletBC(V, no_slip, "on_boundary && near(x[1], 0.0)"),
+        DirichletBC(V.sub(1), free_slip, "on_boundary && near(x[1], 0.4)")
+        ]
 bcp = []
 bcc  = []
 
@@ -157,8 +161,7 @@ a_AdvSink_hat_c = d*u_sink_n_up*c*ds
 
 M_c = assemble(a_M_c)
 D_c = assemble(a_D_c)
-AdvSink_c = assemble(a_AdvSink_c)
-AdvSink_hat_c = assemble(a_AdvSink_hat_c)
+AdvSink_c = assemble(a_AdvSink_c - a_AdvSink_hat_c)
 
 ############################################################
 # store initial conditions
@@ -173,6 +176,7 @@ t_store = dt_store
 # initialise matrix memory
 
 C_m = None
+S_m = None
 b_p_m = None
 b_b_m = None
 b_p = None
@@ -186,10 +190,14 @@ G_c = None
 t = dt
 while t < T:
 
+    loop_timer = Timer("Loop")
+
     k = Constant(dt)
     inv_k = 1.0/dt
 
     nl_it = 0
+    picard_its = []
+
     while nl_it < nl_its:
 
         ############################################################
@@ -206,6 +214,8 @@ while t < T:
             # Define tentative velocity step
             if print_log:
                 print 'Assembling tentative velocity step'
+
+            assembly_timer_0 = Timer("Assembly")
             
             u_nl = theta*u_1 + (1.0-theta)*u_star
             a_C_m = inner(v, grad(u_nl)*u)*dx
@@ -221,42 +231,65 @@ while t < T:
             b_p_m = assemble(L_p_m, tensor=b_p_m)
             b_b_m = assemble(L_b_m, tensor=b_b_m)
 
-            b_m = inv_k*M_m*u_1.vector() - theta*K_m*u_1.vector() - theta*C_m*u_1.vector() - b_p_m + b_b_m
+            b_m = inv_k*M_m*u_1.vector() - theta*K_m*u_1.vector() - \
+                theta*C_m*u_1.vector() - b_p_m + b_b_m
+
+            assembly_timer_0.stop()
 
             # Compute tentative velocity step
             if print_log:
                 print 'Computing tentative velocity step'
+                
+            solver_timer_0 = Timer("Solve")
             
             [bc.apply(A_m, b_m) for bc in bcu]
             solve(A_m, u_0.vector(), b_m)
+                
+            solver_timer_0.stop()
             
             # Define pressure correction
             if print_log:
                 print 'Assembling pressure correction'
 
+            assembly_timer_1 = Timer("Assembly")
+
             L_p = (1./k)*inner(u_0, grad(q))*dx + inner(grad(p_star), grad(q))*dx
             b_p = assemble(L_p, tensor=b_p)
+
+            assembly_timer_1.stop()
 
             # Compute pressure correction
             if print_log:
                 print 'Computing pressure correction'
+                
+            solver_timer_1 = Timer("Solve")
             
             [bc.apply(A_p, b_p) for bc in bcp]
             solve(A_p, p_0.vector(), b_p)
+                
+            solver_timer_1.stop()
 
             # Define velocity correction
             if print_log:
                 print 'Assembling velocity correction'
 
+            assembly_timer_2 = Timer("Assembly")
+
             L_vc = inner(u_0, v)*dx - k*inner(grad(p_0 - p_star), v)*dx
             b_vc = assemble(L_vc, tensor=b_vc)
+
+            assembly_timer_2.stop()
 
             # Compute velocity correction
             if print_log:
                 print 'Computing velocity correction'
+                
+            solver_timer_2 = Timer("Solve")
 
             [bc.apply(A_vc, b_vc) for bc in bcu]
             solve(A_vc, u_0.vector(), b_vc)
+                
+            solver_timer_2.stop()
 
             Eu = errornorm(u_0, u_star, norm_type="L2", degree=shape_U + 1)
 
@@ -266,17 +299,19 @@ while t < T:
             picard_it += 1
 
             if picard_it > 10:
-                print picard_it
-            if picard_it - picard_it/10*10 > 10:
                 print "struggling to converge velocity field. Error=%.2e" % Eu
 
-            # p_0.vector()[:] = p_0.vector().array() - p_0.vector().array()[p_fix]
+            p_0.vector()[:] = p_0.vector().array() - p_0.vector().array()[p_fix]
+
+        picard_its.append(picard_it)
 
         # ADVECTION-DIFFUSION
 
         # Define velocity correction
         if print_log:
             print 'Assembling advection-diffusion equation'
+
+        assembly_timer_3 = Timer("Assembly")
         
         u_0_ta = theta*u_1+(1.0-theta)*u_0
         a_Adv_c = d*div(u_0_ta*c)*dx
@@ -287,7 +322,7 @@ while t < T:
         S_c = assemble(a_S_c, tensor=S_c)
 
         A_c = inv_k*M_c + (1.0-theta)*Adv_c - (1.0-theta)*AdvSink_c + (1.0-theta)*D_c + \
-            (1.0-theta)*S_c + (1.0-theta)*AdvSink_hat_c
+            (1.0-theta)*S_c
 
         # reentrainment
         # tau_b = abs(nu*grad(u_0_ta)*n)
@@ -299,17 +334,28 @@ while t < T:
         # print G_c
 
         b_c = inv_k*M_c*c_1.vector() - theta*Adv_c*c_1.vector() + theta*AdvSink_c*c_1.vector() \
-            - theta*D_c*c_1.vector() - theta*S_c*c_1.vector() - theta*AdvSink_hat_c*c_1.vector() #+ G_c
+            - theta*D_c*c_1.vector() - theta*S_c*c_1.vector() #+ G_c
+
+        assembly_timer_3.stop()
 
         if print_log:
             print 'Compute advection-diffusion of sediment'
+                
+        solver_timer_3 = Timer("Solve")
+
         [bc.apply(A_c, b_c) for bc in bcc]
         solve(A_c, c_0.vector(), b_c)
+                
+        solver_timer_3.stop()
 
         # DEPOSITED SEDIMENT
+                
+        solver_timer_4 = Timer("Solve")
 
         D = d*(c_d_0 - c_d_1)*dx - k*d*u_sink_n_up*c_0_ta*ds
         solve(D == 0, c_d_0)
+                
+        solver_timer_4.stop()
 
         nl_it += 1
 
@@ -318,6 +364,7 @@ while t < T:
     # c_file << c_0
     # c_d_file << c_d_0
 
+    save_timer = Timer("Save")
     if t > t_store:
         # Save to file
         u_file << u_0
@@ -326,6 +373,7 @@ while t < T:
         c_d_file << c_d_0
 
         t_store += dt_store
+    save_timer.stop()
 
     # Store values for next timestep
     u_1.assign(u_0)
@@ -336,4 +384,33 @@ while t < T:
     dt = Calc_timestep(u_0, h)
 
     t += dt
-    print "t=%.5f, dt=%.2e, picard iterations=%d" % (t, dt, picard_it)
+    print "t = %.5f, dt = %.2e, picard iterations =" % (t, dt), picard_its, ", Eu = %.3e" % Eu
+
+    loop_timer.stop()
+    sum_0 = (assembly_timer_0.value()+ 
+              assembly_timer_1.value()+ 
+              assembly_timer_2.value()+ 
+              assembly_timer_3.value())
+    print ("assembly times = %.3e, %.3e, %.3e, %.3e, total = %.3e"  
+           % (assembly_timer_0.value(), 
+              assembly_timer_1.value(), 
+              assembly_timer_2.value(), 
+              assembly_timer_3.value(),
+              sum_0))
+    sum_1 = (solver_timer_0.value()+ 
+              solver_timer_1.value()+ 
+              solver_timer_2.value()+ 
+              solver_timer_3.value()+  
+              solver_timer_4.value())
+    print ("solve times = %.3e, %.3e, %.3e, %.3e, %.3e, total = %.3e"  
+           % (solver_timer_0.value(), 
+              solver_timer_1.value(), 
+              solver_timer_2.value(), 
+              solver_timer_3.value(),  
+              solver_timer_4.value(),
+              sum_1))
+    print ("save time = %.3e, sum of assembly/solve = %.3e, time step = %.3e, lost time = %.3e" 
+           % (save_timer.value(),
+              (sum_0 + sum_1) * nl_its,
+              loop_timer.value(),
+              loop_timer.value() - (sum_0 + sum_1) * nl_its - save_timer.value()))
