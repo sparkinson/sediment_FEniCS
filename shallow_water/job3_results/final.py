0bc3bc8dcb49cca1e81c00cac2016e0ca345cc6b
Samuel Parkinson <samuel.d.parkinson@googlemail.com>
2013-06-13 09:47:58 +0100

    added python diagnostic files

---
+++ shallow_water/job3_results/final.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+from __future__ import unicode_literals
+import json
+from matplotlib.pyplot import *
+from numpy import *
+import matplotlib as mpl
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+
+mpl.rcParams['text.usetex']=True
+mpl.rcParams['text.latex.unicode']=True
+rc('font',**{'family':'serif','serif':['cm']})
+
+class FixedOrderFormatter(ScalarFormatter):
+    """Formats axis ticks using scientific notation with a constant order of 
+    magnitude"""
+    def __init__(self, order_of_mag=0, format=None, useOffset=True, useMathText=False):
+        self._order_of_mag = order_of_mag
+
+        self.set_useOffset(useOffset)
+        self._usetex = rcParams['text.usetex']
+        self._useMathText = useMathText
+        self.orderOfMagnitude = 0
+        self.format = ''
+        self._scientific = True
+        self._powerlimits = rcParams['axes.formatter.limits']
+        if format is None:
+            self._useLocale = rcParams['axes.formatter.use_locale']
+        else:
+            self._useLocale = True
+            self.format = format
+            self.custom_format = True
+
+    def _set_orderOfMagnitude(self, range):
+        """Over-riding this to avoid having orderOfMagnitude reset elsewhere"""
+        self.orderOfMagnitude = self._order_of_mag
+
+    def _set_format(self):
+        # set the format string to format all the ticklabels
+        # The floating point black magic (adding 1e-15 and formatting
+        # to 8 digits) may warrant review and cleanup.
+        if not self.custom_format:
+            locs = (np.asarray(self.locs)-self.offset) / 10**self.orderOfMagnitude+1e-15
+            sigfigs = [len(str('%1.8f'% loc).split('.')[1].rstrip('0')) \
+                           for loc in locs]
+            sigfigs.sort()
+            self.format = '%1.' + str(sigfigs[-1]) + 'f'
+            if self._usetex:
+                self.format = '$%s$' % self.format
+            elif self._useMathText:
+                self.format = '$\mathdefault{%s}$' % self.format
+
+fname = 'phi_ic_2_init.json'
+file = open(fname,'r')
+phi_ic_target = array(json.loads(file.readline()))
+file.close()
+
+fname = 'phi_ic_2.json'
+file = open(fname,'r')
+phi_ic = array(json.loads(file.readline()))
+file.close()
+
+fname = 'deposit_data_2_init.json'
+file = open(fname,'r')
+deposit = array(json.loads(file.readline()))
+file.close()
+
+fname = 'deposit_data_2.json'
+file = open(fname,'r')
+deposit_2 = array(json.loads(file.readline()))
+file.close()
+
+fig = figure(figsize = (8, 3))
+fig.subplots_adjust(wspace=0.4, hspace=0.4, left=0.1, right=0.9, top=0.9, bottom=0.1)
+ax = fig.add_subplot(121, axisbg='w')
+
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$\varphi$')
+# ax.set_xlim([0.0,0.03])
+ax.autoscale(enable=True, axis='x', tight=True)
+fig.tight_layout()
+
+ax.plot(linspace(0,0.5,len(phi_ic_target)),phi_ic_target,'k--')
+ax.plot(linspace(0,0.5,len(phi_ic)),phi_ic,'b-')
+
+# fig.savefig('phi_ic.png')
+
+# fig2 = figure(2,figsize = (6, 3))
+# fig2.subplots_adjust(wspace=0.4, hspace=0.4, left=0.1, right=0.9, top=0.9, bottom=0.1)
+ax = fig.add_subplot(122, axisbg='w')
+
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$\eta$')
+# ax.set_xlim([0.0,0.03])
+ax.autoscale(enable=True, axis='x', tight=True)
+fig.tight_layout()
+
+ax.plot(linspace(0,14.705646497415245,len(deposit)),deposit,'k--')
+ax.plot(linspace(0,12.966874985393511,len(deposit_2)),deposit_2,'b-')
+
+# ax.set_yscale('log')
+# ax.set_xscale('log')
+        
+#show()
+
+# ax.legend(loc=1)
+
+fig.savefig('final.png')
---
+++ shallow_water/verification/verification.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+from __future__ import unicode_literals
+import json
+from matplotlib.pyplot import *
+from numpy import *
+import matplotlib as mpl
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+
+mpl.rcParams['text.usetex']=True
+mpl.rcParams['text.latex.unicode']=True
+rc('font',**{'family':'serif','serif':['cm']})
+
+class FixedOrderFormatter(ScalarFormatter):
+    """Formats axis ticks using scientific notation with a constant order of 
+    magnitude"""
+    def __init__(self, order_of_mag=0, format=None, useOffset=True, useMathText=False):
+        self._order_of_mag = order_of_mag
+
+        self.set_useOffset(useOffset)
+        self._usetex = rcParams['text.usetex']
+        self._useMathText = useMathText
+        self.orderOfMagnitude = 0
+        self.format = ''
+        self._scientific = True
+        self._powerlimits = rcParams['axes.formatter.limits']
+        if format is None:
+            self._useLocale = rcParams['axes.formatter.use_locale']
+        else:
+            self._useLocale = True
+            self.format = format
+            self.custom_format = True
+
+    def _set_orderOfMagnitude(self, range):
+        """Over-riding this to avoid having orderOfMagnitude reset elsewhere"""
+        self.orderOfMagnitude = self._order_of_mag
+
+    def _set_format(self):
+        # set the format string to format all the ticklabels
+        # The floating point black magic (adding 1e-15 and formatting
+        # to 8 digits) may warrant review and cleanup.
+        if not self.custom_format:
+            locs = (np.asarray(self.locs)-self.offset) / 10**self.orderOfMagnitude+1e-15
+            sigfigs = [len(str('%1.8f'% loc).split('.')[1].rstrip('0')) \
+                           for loc in locs]
+            sigfigs.sort()
+            self.format = '%1.' + str(sigfigs[-1]) + 'f'
+            if self._usetex:
+                self.format = '$%s$' % self.format
+            elif self._useMathText:
+                self.format = '$\mathdefault{%s}$' % self.format
+
+fname = 'error.json'
+file = open(fname,'r')
+
+h = array(json.loads(file.readline()))
+
+errors = []
+for line in file.readlines():
+    errors.append(array(json.loads(line)))
+
+fig = figure(figsize = (5, 5))
+fig.subplots_adjust(wspace=0.4, hspace=0.4, left=0.1, right=0.9, top=0.9, bottom=0.1)
+ax = fig.add_subplot(111, axisbg='w')
+
+ax.set_xlabel(r'$cell size$')
+ax.set_ylabel(r'$error$')
+# ax.set_xlim([0.0,0.03])
+ax.autoscale(enable=True, axis='x')#, tight=True)
+fig.tight_layout()
+
+ax.plot(h,errors[0],'r-',label=r'$h$')
+ax.plot(h,errors[2],'b-',label=r'$q$')
+ax.plot(h,errors[1],'g-',label=r'$\varphi$')
+ax.plot(h,errors[3],'y-',label=r'$\eta$')
+ax.plot([0.5,0.25],[0.01, 0.0025],'k--',label=r'$2^{nd}$ order')
+
+
+
+ax.set_yscale('log')
+ax.set_xscale('log')
+
+fig.gca().invert_xaxis()
+        
+#show()
+
+ax.legend(loc=1)
+
+fig.savefig('e.png')
